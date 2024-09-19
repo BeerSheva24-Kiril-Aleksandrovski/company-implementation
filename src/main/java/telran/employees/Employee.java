34cde1fdb12916290401a1d75876305aec816c46
Kiril <fanky.yahoo@gmail.com>
2024-09-19 22:26:39 +0300

    Project created and CW#30 added

---
+++ src/main/java/telran/employees/Employee.java
@@ -0,0 +1,24 @@
+package telran.employees;
+
+public class Employee {
+    private long id;
+    private int basicSalary;
+    private String department;
+    public Employee(long id, int basicSalary, String department) {
+        this.id = id;
+        this.basicSalary = basicSalary;
+        this.department = department;
+    }  
+
+    public int computeSalary() {
+        return basicSalary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+}
